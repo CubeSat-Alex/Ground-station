c7288c33e7843d97fea02fad9a5d45217feac8c6
Abdelmenam-Tarek-Abdelmenam <moneam.elbahy@gmail.com>
2022-09-04 14:58:49 +0200

    database and cache

--- model/database.py
+++ model/database.py
@@ -18,39 +18,95 @@ ldr2 = "ldr2"
 ldr3 = "ldr3"
 ldr4 = "ldr4"
 
-schema = '''
+orbit = "orbit"
+details = "details"
+state = "state"
+
+telemetryShema = f'''
 CREATE TABLE IF NOT EXISTS "telemetry" (
-    "{}"	TEXT,
-	"{}"	NUMERIC,
-	"{}"	NUMERIC,
-	"{}"	NUMERIC,
-	"{}"	NUMERIC,
-	"{}"	NUMERIC,
-	"{}"	NUMERIC,
-	"{}"	NUMERIC,
-	"{}"	NUMERIC,
-	"{}"	NUMERIC,
-	"{}"	NUMERIC,
-	"{}"	NUMERIC,
-	"{}"	NUMERIC,
-	"{}"	NUMERIC,
-	"{}"	NUMERIC
+    "{date}"	TEXT,
+	"{orbit}"	NUMERIC,
+	"{acceleration}"	NUMERIC,
+	"{pressure}"	NUMERIC,
+	"{tempreture}"	NUMERIC,
+	"{altitude}"	NUMERIC,
+	"{angleX}"	NUMERIC,
+	"{angleY}"	NUMERIC,
+	"{angleZ}"	NUMERIC,
+	"{lat}"	NUMERIC,
+	"{lang}"	NUMERIC,
+	"{ldr1}"	NUMERIC,
+	"{ldr2}"	NUMERIC,
+	"{ldr3}"	NUMERIC,
+	"{ldr4}"	NUMERIC
 );
-'''.format(date,orbit, acceleration, pressure,
-           tempreture, altitude,
-           angleX, angleY, angleZ, lat,
-           lang, ldr1, ldr2, ldr3, ldr4)
+'''
+
+logsSchema = f'''
+CREATE TABLE IF NOT EXISTS "logs" (
+	"{date}"	TEXT,
+	"{orbit}"	INTEGER,
+	"{details}"	TEXT,
+	"{state}"	TEXT
+);
+'''
+
+planSchema = f'''
+CREATE TABLE IF NOT EXISTS "plan" (
+	"from"	TEXT,
+	"to"	TEXT,
+    "id"	INTEGER,
+	PRIMARY KEY("id" AUTOINCREMENT)
+);
+'''
 
 
 class DataBase:
     def __init__(self):
         self.connection = sqlite3.connect('database/data.db', check_same_thread=False)
         self.cursor = self.connection.cursor()
-        self.cursor.execute(schema)
+        self.cursor.execute(telemetryShema)
+        self.cursor.execute(logsSchema)
+        self.cursor.execute(planSchema)
         self.connection.commit()
 
     def dispose(self):
         self.connection.close()
+
+    def addPlan(self , plan):
+        query = "insert into plan " + str(tuple(plan.keys())) + " values" + str(
+                tuple(plan.values())) + ";"
+        self.cursor.execute(query)
+        self.connection.commit()
+    
+    def getPlanes(self):
+         df = pd.read_sql_query("SELECT * from plan", self.connection)
+         return df.to_dict('records')
+        
+    def deletePlan(self , id):
+        query = f'''DELETE FROM plan
+                    WHERE id={id};'''
+        self.cursor.execute(query)
+        self.connection.commit()
+
+
+    def addLogs(self, logs):
+        data =logs.split('\n')
+       
+        for index in range(len(data)):
+            dictionary = {date: data[index].split(',')[0],
+                          orbit: data[index].split(',')[1],
+                          details: data[index].split(',')[2],
+                          state: data[index].split(',')[3]}
+            query = "insert into logs " + str(tuple(dictionary.keys())) + " values" + str(
+                tuple(dictionary.values())) + ";"
+            self.cursor.execute(query)
+            self.connection.commit()
+
+    def getLogs(self): 
+        df = pd.read_sql_query("SELECT * from logs", self.connection)
+        df = df[::-1].reset_index()
+        return df
 
     def addData(self, data):
         data = json.loads(str(data))
